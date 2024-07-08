4e246b8103da52fe1674af174df59e4a664176b1
avila-ca <avilacalaf@gmail.com>
2024-07-08 18:58:57 +0200

    rebuilding the all app

--- server/Routes/appRouter.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { loginController } from "./loginController";
-
-const appRouter = Router()
-
-appRouter.post('/login', loginController)
--- server/Routes/loginController.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-
-export const loginController = (req:Request, res:Response) =>{
-    const {name, pass} = req.body
-
-    console.log(name, pass)
-}   
